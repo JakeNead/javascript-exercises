00dedb76a9a248820bb55a7f4a5c5d057105b0d5
Jake Nead <jakenead@gmail.com>
2022-09-03 08:49:04 -0700

    add psuedo code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+let args = Array.from(arguments)
+for (let i = 0; i < args.length; i++) {}
+
+// loop each arr item
+//  if arr item === other arg || arg {pop(arg)}
+// next arr item....
+//return arr
 
 };
 
