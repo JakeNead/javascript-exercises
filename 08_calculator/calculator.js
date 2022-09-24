98f047ca33dd4d39d5b48dbc6e74e63dc9627fbe
Jake Nead <jakenead@gmail.com>
2022-09-24 10:49:12 -0700

    Function passes all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
 const add = function() {
-	
+	return arguments[0]+ arguments[1]
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1]
 };
 
-const sum = function() {
-	
+// const sum = function() {
+// 	return arguments[0].reduce((a, b) => {a + b}, 0)
+// };
+
+const sum = function(arr) {
+  return arr.reduce((a, b) => {
+    return a + b;
+    }, 0)
+}
+
+const multiply = function(arr) {
+  return arr.reduce((a, b) => {
+    return a * b;
+  })
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  let num =n
+  if (n === 0 || n === 1) {
+    return 1 
+  } 
+	for (let i = n; i > 1; i--){
+    num = num * (i-1)
+  }
+  return num
 };
 
 // Do not edit below this line
