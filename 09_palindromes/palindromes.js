819f5b77525317ab0f6151ee3188b7d9eb04a479
Jake Nead <jakenead@gmail.com>
2022-11-08 13:06:20 -0800

    Function passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,18 @@
-const palindromes = function (arguments) {
-    let str = arguments.toLowerCase().split('').filter(a => a.toUpperCase() !== a.toLowerCase())
-    
-return str
-};
+const palindromes = function (arg) {
+    let str = arg.toLowerCase().split('').filter(a => a.toUpperCase() !== a.toLowerCase())
+    let start = 0
+    let end = str.length -1
+    for (let i=0; i<str.length; i++) {
+      if (start > end) {
+        break
+      } else if (str[start] !== str[end]) {
+        return false
+      }
+      start++
+      end--
+      }
+      return true
+    }
 
-console.log(pandindromes('racecar'))
 // Do not edit below this line
 module.exports = palindromes;
