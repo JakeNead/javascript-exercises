3f9f4382db5ca27e7d2683c7c5acf15b9a922313
Jake Nead <jakenead@gmail.com>
2022-11-08 12:35:30 -0800

    Convert to a lowercase array and filter out symbols

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (arguments) {
+    let str = arguments.toLowerCase().split('').filter(a => a.toUpperCase() !== a.toLowerCase())
+    
+return str
 };
 
+console.log(pandindromes('racecar'))
 // Do not edit below this line
 module.exports = palindromes;
